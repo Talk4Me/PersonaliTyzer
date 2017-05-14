9138f04b061b70bedc05afe86806f05d6fb2ae89
stevefsswanson <swanson.stev@gmail.com>
2017-05-14 04:32:19 -0400

    adding in Needs

--- PersonaliTyzer.py
+++ PersonaliTyzer.py
@@ -26,6 +26,7 @@ def Analyze(body):
     try:
         response = requests.post(url, headers={'Content-Type': 'text/plain;charset=utf-8'}, auth=(username, password), data=text)
         personality = json.loads(response.content)['personality']
+        needs = json.loads(response.content)['needs']
     except Exception as e: 
         print (str(e))
         print type(e)
@@ -35,6 +36,8 @@ def Analyze(body):
 
     for p in personality:
         synthPers[p['name']] = p['percentile']
+    for n in needs:
+        synthPers[n['name']] = n['percentile']
 
     for key in synthPers:
         print(key + ': ' + str(synthPers[key]))
@@ -46,7 +49,7 @@ def dist(dict1, dict2):
     distance = 0
 
     for key in dict1:
-        distance += math.pow(float(dict1[key]) - float(dict2[key]), 2)
+        distance += math.pow(float(dict1[key]) - float(dict2[key]), 4)
 
     return math.pow(distance, .5)
     
