1cb12f69e92070a132c71dd73519fe3667e330d8
EC2 Default User <ec2-user@ip-172-31-13-247.us-west-2.compute.internal>
2017-05-14 11:53:18 +0000

    now its in the url

--- PersonaliTyzer.py
+++ PersonaliTyzer.py
@@ -5,26 +5,26 @@ import math
 from flask import request
 app = Flask(__name__)
 
-@app.route('/PersonaliTyzer', methods=['POST'])
+@app.route('/PersonaliTyzer', methods=['GET'])
 def HandleCall():
-    print(request.data)
-    jsonRequest = json.loads(request.data)
-    print('parsed Json')
-    return str(Analyze(jsonRequest))
+    print(request.__dict__)
+#    print(request.__dict__)['environ']['werkzeug.request'].__dict__
+    #jsonRequest = json.loads(request.data)
+    print(request.query_string)
+    text = request.args.get('text')
+    vector = request.args.get('vector')
+    return str(Analyze(text, {'Curiosity': 1}))
     
 
-def Analyze(body):
-    text = body['text']
-    
+def Analyze(text, vector):
     print(text)
-
-    vector = body['vector']
     username = '9fcf0840-1315-4b0b-a448-8cf5b6ceb510'
     password = 'SBPqWCzEcpTK'
     url = 'https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2016-10-20'
 
     try:
         response = requests.post(url, headers={'Content-Type': 'text/plain;charset=utf-8'}, auth=(username, password), data=text)
+#        print(response.__dict__)
         personality = json.loads(response.content)['personality']
         needs = json.loads(response.content)['needs']
     except Exception as e: 
@@ -49,7 +49,7 @@ def dist(dict1, dict2):
     distance = 0
 
     for key in dict1:
-        distance += math.pow(float(dict1[key]) - float(dict2[key]), 4)
+        distance += math.pow(dict1[key] - dict2[key], 4)
 
     return math.pow(distance, .5)
     
